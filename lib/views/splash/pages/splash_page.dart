12b48836e864ff9f23afec8216b3d6dd15750327
Carlos Mendez <carlosrafael-mg@hotmail.com>
2025-02-25 23:39:26 +0100

    Fixed some errors that prevented the app from initiating properly
    Signed-off-by: Carlos Mendez <carlosrafael-mg@hotmail.com>

--- lib/views/splash/pages/splash_page.dart
+++ lib/views/splash/pages/splash_page.dart
@@ -1,5 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pami/application/authentication/authentication/authentication_bloc.dart';
+import 'package:pami/views/core/routes/router.gr.dart';
 
 /// The Splash Page widget
 /// Checks for authentication and routes the user to either
@@ -10,9 +13,23 @@ class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
-  Widget build(BuildContext context) => const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator.adaptive(),
+  Widget build(BuildContext context) =>
+      BlocListener<AuthenticationBloc, AuthenticationState>(
+        listener: _listener,
+        child: const Scaffold(
+          body: Center(
+            child: CircularProgressIndicator.adaptive(),
+          ),
+        ),
+      );
+
+  void _listener(BuildContext context, AuthenticationState state) => state.when(
+        initial: () => null,
+        authenticated: (_) => context.router.replace(
+          const HomeRoute(),
+        ),
+        unAuthenticated: () => context.router.replace(
+          const LoginRoute(),
         ),
       );
 }
