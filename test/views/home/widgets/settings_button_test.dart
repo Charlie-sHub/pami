b04e9b79f2dc52caff392f5ae351467a74146d39
Carlos Mendez <carlosrafael-mg@hotmail.com>
2025-03-20 16:44:57 +0100

    Finished with the tests
    Signed-off-by: Carlos Mendez <carlosrafael-mg@hotmail.com>

--- test/views/home/widgets/settings_button_test.dart
+++ test/views/home/widgets/settings_button_test.dart
@@ -1,22 +1,58 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:pami/views/core/routes/router.gr.dart';
+import 'package:pami/views/home/widgets/settings_button.dart';
 
-/// Settings button widget
-class SettingsButton extends StatelessWidget {
-  /// Default constructor
-  const SettingsButton({super.key});
+import 'settings_button_test.mocks.dart';
 
-  @override
-  Widget build(BuildContext context) => IconButton(
-        padding: EdgeInsets.zero,
-        icon: const Icon(
-          Icons.settings_rounded,
-          size: 25,
-          // color: WorldOnColors.accent,
-        ),
-        onPressed: () => context.router.push(
-          const SettingsRoute(),
+@GenerateNiceMocks([
+  MockSpec<StackRouter>(),
+])
+void main() {
+  late MockStackRouter mockRouter;
+
+  setUp(
+    () => mockRouter = MockStackRouter(),
+  );
+
+  Widget buildWidget() => MaterialApp(
+        home: StackRouterScope(
+          controller: mockRouter,
+          stateHash: 0,
+          child: const Scaffold(
+            body: SettingsButton(),
+          ),
         ),
       );
+
+  testWidgets(
+    'SettingsButton renders correctly',
+    (tester) async {
+      // Act
+      await tester.pumpWidget(buildWidget());
+
+      // Assert
+      expect(find.byType(IconButton), findsOneWidget);
+      expect(find.byIcon(Icons.settings_rounded), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'SettingsButton navigates to SettingsRoute on tap',
+    (tester) async {
+      // Arrange
+      when(mockRouter.push(any)).thenAnswer((_) async => null);
+
+      // Act
+      await tester.pumpWidget(buildWidget());
+      await tester.tap(find.byType(IconButton));
+      await tester.pumpAndSettle();
+
+      // Assert
+      verify(mockRouter.push(const SettingsRoute())).called(1);
+    },
+  );
 }
